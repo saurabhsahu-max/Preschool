4bfb06f7c8efefcbe048fd1cde1fe5b9b0973a63
Saurabh Sahu <sahu2408saurabh@gmail.com>
2026-02-28 17:40:33 +0530

    Home Page

---
+++ src/test/java/com/preschool/PreschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
